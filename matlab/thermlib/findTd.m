a067bd6f6b63b830fd844ff1e96a254cff1ad1d6
Phil Austin <paustin@eos.ubc.ca>
2012-01-22 12:58:37 -0800

    tweak comments and add end statement

--- matlab/thermlib/findTd.m
+++ matlab/thermlib/findTd.m
@@ -3,7 +3,8 @@ function result= findTd(e0);
   %http://www.mathworks.com/access/helpdesk/help/techdoc/math/f2-116732.html#brfg601-2
   shortHandle=@(Temp) ezero(Temp,e0);
   result=fzero(shortHandle,[200,400]);
-  
+end
+
 function result = ezero(Tguess,e0);
 % ezero evaluates the equation esat(Tguess) - e0 = 0
 % and returns it to the root finder
@@ -11,3 +12,4 @@ function result = ezero(Tguess,e0);
    %when this difference is small enough we're done
    %fprintf('inside ezero rootfinder, Tguess= %f\n',Tguess);
    result=esat(Tguess) - e0;
+end
--- matlab/thermlib/theta.m
+++ matlab/thermlib/theta.m
@@ -8,6 +8,8 @@ function [thetaout]=theta(varargin);
     %                wv = vapor mixing ratio in kg/kg
     % out:    theta = potential temperature in Kelvin
     % reference emanuel p. 111 4.2.11
+    % this is slightly more accurate than W&H 3.54 because it
+    % uses the heat capacity of the air/vapor mixture
     c=constants;
     if nargin==2
       wv=0;
@@ -20,3 +22,4 @@ function [thetaout]=theta(varargin);
     p=varargin{2};
     power = c.Rd/c.cpd*(1. - 0.24*wv);
     thetaout=T*(c.p0/p)^power;
+end
