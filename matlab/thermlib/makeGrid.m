dd5a66f411445ef36a6440f99a8e6a2546635de3
Phil Austin <paustin@eos.ubc.ca>
2012-01-27 09:55:49 -0800

    makeSkew.m

---
+++ matlab/thermlib/makeGrid.m
@@ -0,0 +1,37 @@
+function [xplot,yplot,temp,thetaVals,thetaeVals,ws,skew]=makeGrid()
+%make the grid for a tephigram
+%get a dense range of p, t0 to contour
+    c=constants;
+    yplot=[1000:-10:100];
+    xplot=[-300:1:-140];
+    [rows,pvals]=size(yplot);
+    [rows,tvals]=size(xplot);
+    [pvals,tvals]
+    temp=zeros([pvals,tvals]);
+    thetaVals=zeros([pvals,tvals]);
+    thetaeVals=zeros([pvals,tvals]);
+    ws=zeros([pvals,tvals]);
+    skew=30; %skewness factor (deg C)
+    %lay down a reference grid that labels xplot,yplot points 
+    %in the new (skewT-lnP) coordinate system .
+    % Each value of the temp matrix holds the actual (data) temperature
+    % label (in deg C)  of the xplot, yplot coordinate pairs
+    for i=1:pvals,
+       for j=1:tvals,
+          %note that we don't have to transform the y coordinate
+          %it's still the pressure
+          temp(i,j)=convertSkewToTemp(xplot(j),yplot(i),skew);
+          Tk=temp(i,j) + c.Tc; %convert from C to K for use in
+                                 %thermo functios
+          %use the real (data) value to get the potential temperature
+          %theta function needs T in (K) and press in Pa
+          press=yplot(i)*100.; %convert from hPa to Pa
+          thetaVals(i,j)=theta(Tk,press); %theta labels
+          %add the mixing ratio
+          ws(i,j)=wsat(Tk,press);  %wsat labels
+          %saturated adiabat, so Tdew=Tk
+          thetaeVals(i,j)=thetaep(Tk,Tk,press); %thetae labels
+       end
+    end
+fprintf('through makeGrid\n')
+end
---
+++ matlab/thermlib/makeSkew.m
@@ -0,0 +1,37 @@
+%make a blank skewT diagram
+function [figureHandle,skew]=makeSkew(figHandle)
+    c=constants;
+    %draw a skewT diagram and return the figure handle
+    %and skew so other lines can be added later
+    figureHandle=figure(figHandle);
+    clf;
+    [xplot,yplot,temp,thetaVals,thetaeVals,ws,skew]=makeGrid();
+    tempLabels= -40:5:40;
+    [output,hand]=contour(xplot,yplot,temp,tempLabels,'k');
+    clabel(output,hand);
+    set(gca,'yscale','log','ydir','reverse');
+    set(gca,'fontweight','bold');
+    %draw 10 horizontal pressure lines using ygrid
+    set(gca,'ytick',[100:100:1000]);
+    set(gca,'ygrid','on');
+    hold on;  %this locks axes for adding more lines later
+    thetaLabels=200:5:340;
+    [output,handle]=contour(xplot,yplot,thetaVals,thetaLabels,'b');
+    clabel(output,handle);
+    title('skew T - lnp chart');
+    ylabel('pressure (hPa)');
+    xlabel('temperature (black, degrees C)');
+    wsLabels=[4:2:20,24,28]
+    [output,handle]=contour(xplot,yplot,ws*1.e3,wsLabels,'g');
+    clabel(output,handle);
+    thetaeLabels= [300,310,320,330,340,350,360,380,400]
+    [output,handle]=contour(xplot,yplot,thetaeVals,thetaeLabels,'r');
+    clabel(output,handle); 
+    TempTickLabels=-25:5:30;
+    TempTickCoords=TempTickLabels;
+    skewTickCoords=convertTempToSkew(TempTickCoords,1.e3,skew);
+    set(gca,'xtick',skewTickCoords);
+    set(gca,'xticklabel',TempTickLabels);
+    skewLimits=convertTempToSkew([0,30],1.e3,skew);
+    axis([skewLimits(1),skewLimits(2),600,1.e3]);
+end
